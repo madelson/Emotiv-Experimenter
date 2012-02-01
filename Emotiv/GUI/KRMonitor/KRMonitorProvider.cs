3db498d6dca98fec53704155fcf6f5c849b04200
Michael Adelson <mike.adelson314@gmail.com>
2012-01-31 20:03:29 -0500

    Added NOTE MA comments to KRMonitorProvider.cs.
    These comments should help resolve some of the issues with the provider.

--- Emotiv/GUI/KRMonitor/KRMonitorProvider.cs
+++ Emotiv/GUI/KRMonitor/KRMonitorProvider.cs
@@ -41,9 +41,9 @@ namespace MCAEmotiv.GUI.KRMonitor
             for (int i = 0; i < settings.NumRounds; i++)
             {
                 yield return new ChoiceView(new string[] 
-            { 
-                "Start Study Phase"
-            }, out result) { Text = "Click When Ready" };
+                { 
+                    "Start Study Phase"
+                }, out result) { Text = "Click When Ready" };
 
                 foreach (var stimulus in presentation)
                 {
@@ -51,9 +51,9 @@ namespace MCAEmotiv.GUI.KRMonitor
                     yield return new RestView(this.settings.RestTime);
                 }
                 yield return new ChoiceView(new string[] 
-            { 
-                "Start Test Phase"
-            }, out result) { Text = "Click When Ready" };
+                { 
+                    "Start Test Phase"
+                }, out result) { Text = "Click When Ready" };
 
                 var connected = true; // assume it's connected
                 using (var invoker = new SingleThreadedInvoker())
@@ -75,13 +75,16 @@ namespace MCAEmotiv.GUI.KRMonitor
             }
         }
 
-
-
-
         private IEnumerable<View> GetViews(ISynchronizeInvoke invoker)
         {
             var currentTrialEntries = new List<EEGDataEntry>();
             //To do: Save the date/time earlier and use it for both this and the dataWriter. Put it in GetEnumerator and pass to GetViews
+            /*
+             * NOTE MA 1/31/12 we're creating a new logWriter on each call to GetViews, which gets called multiple times in GetEnumerator (its in a loop)
+             * to avoid creating multiple files, we can move this using statement into GetEnumerator (outside the loop) and pass it in as an argument
+             * to GetViews instead. The same goes for the dataWriter. If you feel like you're passing around too many arguments, feel free to lump them
+             * together in a Runtime object like I do in ExperimentProvider.cs
+             */
             using (var logWriter = new StreamWriter(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "krmon_log_" + settings.SubjectName + "_" + DateTime.Now.ToString("MM dd yyyy H mm ss") + ".txt")))
             using (var dataWriter = new StreamWriter(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "krmon_" + settings.SubjectName + "_" + DateTime.Now.ToString("MM dd yyyy H mm ss") + ".csv")))
             using (var artifactListener = new EEGDataListener(invoker, null, data =>
@@ -103,13 +106,29 @@ namespace MCAEmotiv.GUI.KRMonitor
                 //DIFFERENCE: Instead of getting blocks, we want to go through the entire set once for each test phase, but we
                 //want items within that set to be random
                 RandomizedQueue<StudyTestPair> usedPairs = new RandomizedQueue<StudyTestPair>();
+                /* 
+                 * NOTE MA 1/31/12 this loop seems a little sketchy. We're ostensibly counting from 0 to stp.Count, but stp.Count
+                 * will decrement on each call to RemoveRandom(), so we'll only get half of our stimuli. Consider while (this.stp.Count > 0)
+                 * or int count = this.stp.Count; for (int i = 0; i < count; i++)
+                 */
+                /*
+                 * NOTE MA 1/31/12 another thing to note here is that you probably don't want to modify stp because it is global to the provider,
+                 * not just to this particular run. That means that you risk getting into an inconsistent state if the run restarts or hits an error.
+                 * It would probably better to make this.stp an IArrayView (which cannot be modified) and do:
+                 * 
+                 * var stimulusPairs = new RandomizedQueue<StudyTestPair>();
+                 * stimulusPairs.AddRange(this.stp);
+                 * 
+                 * and then work with stimulusPairs.
+                 */
                 for (int index = 0; index < this.stp.Count; index++)
                 {
                     StudyTestPair currstp = stp.RemoveRandom();
                     usedPairs.Add(currstp);
                     logWriter.WriteLine("Question: " + currstp.test);
                     logWriter.WriteLine("Correct Answer: " + currstp.answer);
-                    foreach (var view in RunTrial(index, currstp.test, currstp.answer, dataWriter, logWriter, currentTrialEntries)) {
+                    foreach (var view in RunTrial(index, currstp.test, currstp.answer, dataWriter, logWriter, currentTrialEntries))
+                    {
                         yield return view;
                     }
 
@@ -154,7 +173,7 @@ namespace MCAEmotiv.GUI.KRMonitor
             });
             yield return vocabView;
             //DIFFERENCE: provide the user and the log with feedback 
-            yield return new TextView((bool) result.Value ? "Correct" : "Incorrect", 2000);
+            yield return new TextView((bool)result.Value ? "Correct" : "Incorrect", 2000);
             logWriter.WriteLine("User Answer: " + result.Value);
             if (needToRerun)
             {
